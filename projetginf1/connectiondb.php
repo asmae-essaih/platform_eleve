cbb652fc3e759adcc7b67a136b560f53fe68749e
asmae-essaih <96588707+asmae-essaih@users.noreply.github.com>
2022-02-12 15:36:26 +0100

    Update connectiondb.php

--- projetginf1/connectiondb.php
+++ projetginf1/connectiondb.php
@@ -1,4 +1,4 @@
 
 <?php
-$connect = mysqli_connect('localhost', 'root', 'root', 'ensat') or die("Impossible de se connecter à la base de données");
+$connect = mysqli_connect('localhost', 'root', '', 'ensat') or die("Impossible de se connecter à la base de données");
 ?>
